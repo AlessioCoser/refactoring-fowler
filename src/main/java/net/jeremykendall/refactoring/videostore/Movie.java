89550bbf7f8667324d9e07d509ab89cca2d66512
Marco Dallagiacoma <marco@Marcos-MBP.homenet.telecomitalia.it>
2018-03-20 12:57:54 +0100

    Use Price class to store price code

--- src/main/java/net/jeremykendall/refactoring/videostore/Movie.java
+++ src/main/java/net/jeremykendall/refactoring/videostore/Movie.java
@@ -28,7 +28,7 @@ public class Movie {
     public static final int NEW_RELEASE = 1;
 
     private String _title;
-    private int _priceCode;
+    private Price _price;
 
     public Movie(String title, int priceCode) {
         _title = title;
@@ -62,11 +62,23 @@ public class Movie {
     }
 
     public int getPriceCode() {
-        return _priceCode;
+        return _price.getPriceCode();
     }
 
-    public void setPriceCode(int _priceCode) {
-        this._priceCode = _priceCode;
+    public void setPriceCode(int arg) {
+        switch (arg) {
+            case REGULAR:
+                _price = new RegularPrice();
+                break;
+            case CHILDRENS:
+                _price = new ChildrensPrice();
+                break;
+            case NEW_RELEASE:
+                _price = new NewReleasePrice();
+                break;
+            default:
+                throw new IllegalArgumentException("Incorrect Price Code");
+        }
     }
 
     public String getTitle() {
