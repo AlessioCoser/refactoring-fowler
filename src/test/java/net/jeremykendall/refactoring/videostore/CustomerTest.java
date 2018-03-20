847633af656883d7ab58b7d2f7ba0694c478ce81
Alessio Coser <alessio.coser@xpeppers.com>
2018-03-20 10:39:53 +0100

    Test one day rental of regular movie

--- src/test/java/net/jeremykendall/refactoring/videostore/CustomerTest.java
+++ src/test/java/net/jeremykendall/refactoring/videostore/CustomerTest.java
@@ -13,4 +13,18 @@ public class CustomerTest {
                 "You earned 0 frequent renter points";
         assertEquals(expected, customer.statement());
     }
+
+    @Test
+    public void OneRegularMovieForOneDayShouldOweTwo() {
+        Customer customer = new Customer("customerName");
+        Movie regularMovie = new Movie("movieTitle", Movie.REGULAR);
+
+        customer.addRental(new Rental(regularMovie, 1));
+
+        String expected = "Rental Record for customerName\n" +
+                "\tmovieTitle\t2.0\n" +
+                "Amount owed is 2.0\n" +
+                "You earned 1 frequent renter points";
+        assertEquals(expected, customer.statement());
+    }
 }
