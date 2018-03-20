fdd363ec03285f4cf8eed2e0ae4811d4998ba459
Alessio Coser <alessio.coser@xpeppers.com>
2018-03-20 10:47:27 +0100

    Add test for rental for more than two days

--- src/test/java/net/jeremykendall/refactoring/videostore/CustomerTest.java
+++ src/test/java/net/jeremykendall/refactoring/videostore/CustomerTest.java
@@ -15,7 +15,7 @@ public class CustomerTest {
     }
 
     @Test
-    public void OneRegularMovieForOneDayShouldOweTwo() {
+    public void OneRegularMovieForLessThanTwoDaysShouldOweTwo() {
         Customer customer = new Customer("customerName");
         Movie regularMovie = new Movie("movieTitle", Movie.REGULAR);
 
@@ -24,6 +24,20 @@ public class CustomerTest {
         String expected = "Rental Record for customerName\n" +
                 "\tmovieTitle\t2.0\n" +
                 "Amount owed is 2.0\n" +
+                "You earned 1 frequent renter points";
+        assertEquals(expected, customer.statement());
+    }
+
+    @Test
+    public void OneRegularMovieForMoreThanTwoDaysShouldAddOneAndHalfADay() {
+        Customer customer = new Customer("customerName");
+        Movie regularMovie = new Movie("movieTitle", Movie.REGULAR);
+
+        customer.addRental(new Rental(regularMovie, 3));
+
+        String expected = "Rental Record for customerName\n" +
+                "\tmovieTitle\t3.5\n" +
+                "Amount owed is 3.5\n" +
                 "You earned 1 frequent renter points";
         assertEquals(expected, customer.statement());
     }
