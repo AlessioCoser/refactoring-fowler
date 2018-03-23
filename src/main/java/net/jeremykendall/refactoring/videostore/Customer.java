8a01607e80b82e19a19de41cbbb8e33bade77940
Marco Dallagiacoma <marco@Marcos-MBP.homenet.telecomitalia.it>
2018-03-23 10:43:48 +0100

    Extract method getFrequentRenterPoints

--- src/main/java/net/jeremykendall/refactoring/videostore/Customer.java
+++ src/main/java/net/jeremykendall/refactoring/videostore/Customer.java
@@ -13,7 +13,6 @@ public class Customer {
 
     public String statement() {
         double totalAmount = 0;
-        int frequentRenterPoints = 0;
         Enumeration rentals = _rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
         while (rentals.hasMoreElements()) {
@@ -21,22 +20,34 @@ public class Customer {
 
             double thisAmount = getRentalCharge(each);
 
-            // add frequent renter points
-            frequentRenterPoints ++;
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-                    each.getDaysRented() > 1) frequentRenterPoints ++;
-
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle()+ "\t" +
                     String.valueOf(thisAmount) + "\n";
             totalAmount += thisAmount;
         }
+
+        int frequentRenterPoints = getFrequentRenterPoints();
+
         //add footer lines
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) +
                 " frequent renter points";
         return result;
+    }
+
+    private int getFrequentRenterPoints() {
+        int frequentRenterPoints = 0;
+        Enumeration rentals = _rentals.elements();
+
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+
+            frequentRenterPoints ++;
+            // add bonus for a two day new release rental
+            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+                    each.getDaysRented() > 1) frequentRenterPoints ++;
+        }
+        return frequentRenterPoints;
     }
 
     private double getRentalCharge(Rental rental) {
